1f8bbc25aa100b73a34818d36e346e5126f210c6
andrew-dahlmanoeth <andrew.dahlmanoeth@digipen.edu>
2022-01-19 13:15:41 -0800

    Background scroll

--- Assets/Scripts/BackgroundMovement.cs
+++ Assets/Scripts/BackgroundMovement.cs
@@ -4,17 +4,23 @@ using UnityEngine;
 
 public class BackgroundMovement : MonoBehaviour
 {
+    public int yConstraint = 10;
+    private Rigidbody2D bgRB;
+    private Vector2 speed = new Vector2(0, -5);
 
-    public GameObject background;
     // Start is called before the first frame update
     void Start()
     {
-        
+        bgRB = GetComponent<Rigidbody2D>();
+        bgRB.velocity = speed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(transform.position.y < -yConstraint)
+        {
+            transform.position = new Vector2(0, yConstraint);
+        }
     }
 }

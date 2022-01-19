09d7fa019aabb76a0608ec05a237d061af66f516
melaniebaloban <melanie.baloban@digipen.edu>
2022-01-19 14:23:37 -0800

    generated obstacles

--- Assets/Scripts/GeneratedObstacles.cs
+++ Assets/Scripts/GeneratedObstacles.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class GeneratedObstacles : MonoBehaviour
 {
+    public GameObject obstacle;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(GenerateObstacle());
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
+    }
+
+    IEnumerator GenerateObstacle()
+    {
+        float maxRange = Random.Range(-7, 7);
+ 
+        yield return new WaitForSeconds(2);
+        Instantiate(obstacle, new Vector3(maxRange, 4, 0), Quaternion.identity);
         
+        StartCoroutine(GenerateObstacle());
     }
 }

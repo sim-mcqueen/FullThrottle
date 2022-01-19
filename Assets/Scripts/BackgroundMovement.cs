75be09435df014e4fb22d3a248a69a91a8755912
sim-mcqueen <s.mcqueen@digipen.edu>
2022-01-19 13:38:58 -0800

    commit

--- Assets/Scripts/BackgroundMovement.cs
+++ Assets/Scripts/BackgroundMovement.cs
@@ -6,7 +6,7 @@ public class BackgroundMovement : MonoBehaviour
 {
     public float yConstraint = 10.4f;
     private Rigidbody2D bgRB;
-    private Vector2 speed = new Vector2(0, -5);
+    public Vector2 speed = new Vector2(0, -5);
     private Vector2 startPos = new Vector2(0, 10.4f);
 
     void Start()

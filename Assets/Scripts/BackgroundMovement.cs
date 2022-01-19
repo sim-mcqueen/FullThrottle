eb0ecfc2ade9fb990c14a665c27c718a4d60f812
andrew-dahlmanoeth <andrew.dahlmanoeth@digipen.edu>
2022-01-19 13:40:17 -0800

    Changed speed from Vec to Int

--- Assets/Scripts/BackgroundMovement.cs
+++ Assets/Scripts/BackgroundMovement.cs
@@ -1,4 +1,9 @@
-﻿using System.Collections;
+﻿//////////////////////////////
+//// Name: Andrew Dahlman-Oeth
+//// Date: 1/19/22
+//// Desc: Infinite background scroll
+//////////////////////////////
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,13 +11,13 @@ public class BackgroundMovement : MonoBehaviour
 {
     public float yConstraint = 10.4f;
     private Rigidbody2D bgRB;
-    public Vector2 speed = new Vector2(0, -5);
+    public float speed = 5f;
     private Vector2 startPos = new Vector2(0, 10.4f);
 
     void Start()
     {
         bgRB = GetComponent<Rigidbody2D>();
-        bgRB.velocity = speed;
+        bgRB.velocity = new Vector2(0, speed);
     }
 
     private void OnBecameInvisible()

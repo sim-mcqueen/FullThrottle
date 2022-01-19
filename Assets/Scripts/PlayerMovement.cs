08112857ba60aeb80953b040db2993fa3e4f2172
sim-mcqueen <s.mcqueen@digipen.edu>
2022-01-19 12:58:34 -0800

    Added player movement script

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿//////////////////////////////
+//// Name: Simeon McQueen
+//// Date: 1/19/22
+//// Desc: Script that controllers the car
+//////////////////////////////
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

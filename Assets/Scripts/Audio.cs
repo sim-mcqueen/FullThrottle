dabce760266760cb105065244db46b0c08f09e0e
andrew-dahlmanoeth <andrew.dahlmanoeth@digipen.edu>
2022-01-24 13:52:06 -0800

    Updated volume to work ingame

--- Assets/Scripts/Audio.cs
+++ Assets/Scripts/Audio.cs
@@ -7,8 +7,13 @@ public class Audio : MonoBehaviour
 {
     public AudioMixer mixer;
 
+    private void Awake()
+    {
+        mixer.SetFloat("MasterVolume", PlayerPrefs.GetFloat("MasterVolume"));
+    }
     public void SetLevel(float sliderValue)
     {
         mixer.SetFloat("MasterVolume", sliderValue);
+        PlayerPrefs.SetFloat("MasterVolume", sliderValue);
     }
 }

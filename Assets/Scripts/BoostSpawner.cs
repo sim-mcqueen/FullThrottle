c0146ec96b100e8d5d9b6c784818a99c495ee96b
sim-mcqueen <s.mcqueen@digipen.edu>
2022-01-26 14:49:52 -0800

    Added boost, however they currently function the same as obstacles

---
+++ Assets/Scripts/BoostSpawner.cs
@@ -0,0 +1,33 @@
+﻿//////////////////////////////
+//// Name: Melanie Baloban
+//// Date: 1/20/22
+//// Desc: Obstacles generate and move down the screen
+//////////////////////////////
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoostSpawner : MonoBehaviour
+{
+    public GameObject boost;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(GenerateBoost());
+
+    }
+
+
+    IEnumerator GenerateBoost()
+    {
+        //left and right bounds of road
+        float maxRange = Random.Range(-3, 3);
+        int timeInBetweenBoost = Random.Range(3, 9);
+        yield return new WaitForSeconds(timeInBetweenBoost);
+
+        Instantiate(boost, new Vector3(maxRange, 9, 0), Quaternion.identity);
+
+        StartCoroutine(GenerateBoost());
+    }
+}
